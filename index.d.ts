06b1184204174087ee7b62abf6934bc388fdd2b8
srxboys <srxboys@126.com>
2020-01-14 14:18:23 +0800

    0.0.3 [mod] update d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,21 +1,30 @@
 declare module 'react-native-rxemitter' {
   export class RXEmitter {
-    /**  */
+    /**
+     * @add
+     * in `componentDidMount`
+     */
     static addListener: (
-      ref: JSX.Element, 
+      ref: any, //not null
       name: string, 
       callback: function
     ) => void;
 
-    /**  */
+    /** 
+     * @remove
+     * in anywhere
+     */
     static removeLister: (
-      ref: JSX.Element, 
+      ref: any, //not null
       name?: string, 
     ) => void;
 
-    /**  */
+    /** 
+     * @emit @send
+     * in anywhere
+     */
     static emit: (
-      name: string, 
+      name: string, //not null
       obj?: any  //any
     ) => void;
 
